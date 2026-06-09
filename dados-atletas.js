b542f54b2771f88c45e2f04c94bfa73297ebddfa
Klausmain <kuhnk7008@gmail.com>
2026-06-08 23:04:30 -0300

    Add Atleta class with various methods

---
+++ dados-atletas.js
@@ -0,0 +1,59 @@
+class Atleta {
+  constructor(nome, idade, peso, altura, notas) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+
+  calculaCategoria() {
+    if (this.idade <= 11) return "Infantil";
+    if (this.idade <= 13) return "Juvenil";
+    if (this.idade <= 15) return "Intermediário";
+    if (this.idade <= 30) return "Adulto";
+    return "Sem categoria";
+  }
+
+  calculaIMC() {
+    return this.peso / (this.altura * this.altura);
+  }
+
+  calculaMediaValida() {
+    let notas = [...this.notas].sort((a, b) => a - b);
+    return (notas[1] + notas[2] + notas[3]) / 3;
+  }
+
+  
+  obtemNomeAtleta() {
+    return this.nome;
+  }
+
+  obtemIdadeAtleta() {
+    return this.idade;
+  }
+
+  obtemPesoAtleta() {
+    return this.peso;
+  }
+
+  obtemAlturaAtleta() {
+    return this.altura;
+  }
+
+  obtemNotasAtleta() {
+    return this.notas;
+  }
+
+  obtemCategoria() {
+    return this.calculaCategoria();
+  }
+
+  obtemIMC() {
+    return this.calculaIMC();
+  }
+
+  obtemMediaValida() {
+    return this.calculaMediaValida();
+  }
+}
